e4ee6b495813d0e146027651e9112e70cbc75282
Niko Auer <nikoauer26@gmail.com>
2024-01-10 15:22:09 +1100

    error page added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom"
+
 export default function Error() {
     return (
-        <h1>404 page not found</h1>
+        <html className="h-full">
+  <body className="flex h-full">
+    <div className="max-w-[50rem] flex flex-col mx-auto w-full h-full">
+      <header className="mb-auto flex justify-center z-50 w-full py-4 my-10">
+        <nav className="px-4 sm:px-6 lg:px-8 n" aria-label="Global">
+          <Link className="flex-none text-xl font-semibold sm:text-3xl" to="/" aria-label="Brand">Niko Auer</Link>
+        </nav>
+      </header>
+
+      <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+        <h1 className="block text-7xl font-bold text-gray-800 sm:text-9x">404</h1>
+        <h1 className="block text-2xl font-bold text-white"></h1>
+        <p className="mt-3 text-gray-600">Oops, something went wrong.</p>
+        <p className="text-gray-600">Sorry, we couldn't find your page.</p>
+        <button type="button" className="py-3 my-5 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+  <Link to="/">Home</Link>
+</button>
+      </div>
+
+      <footer className="mt-auto text-center py-5">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <p className="text-sm text-gray-500">All right reservered to Niko Auer</p>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import React from "react";
 import logo from '../assets/logo3.svg'
 
 
