2f5f253fc24ba75e14831150c99eb56e5241871a
Niko Auer <nikoauer26@gmail.com>
2024-01-10 21:34:45 +1100

    added content to about me page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,5 +1,375 @@
+import portrait from "../assets/portrait2.jpeg";
+import bootstrap from "../assets/bootstrap.svg"
+import express from "../assets/express.svg"
+import figma from "../assets/figma.svg"
+import git from "../assets/git.svg"
+import css from "../assets/css.svg"
+import github from "../assets/github.svg"
+import heroku from "../assets/heroku.svg"
+import html from "../assets/html.svg"
+import javascript from "../assets/javascript.svg"
+import jest from "../assets/jest.svg"
+import jquery from "../assets/jquery.svg"
+import mongodb from "../assets/mongodb.svg"
+import mysql from "../assets/mySQL.svg"
+import netlify from "../assets/netlify.svg"
+import react from "../assets/react.svg"
+import Sass from "../assets/Sass.svg"
+import Tailwind from "../assets/tailwind.svg"
+import graphql from "../assets/graphql.svg"
+import redux from "../assets/redux.svg"
+
+
 export default function AboutPage() {
-    return(
-        <h1>About</h1>
-    )
+  return (
+    <>
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="md:grid md:grid-cols-2 md:gap-10 lg:gap-16 md:items-center">
+          <div className="hidden md:block mb-24 md:mb-0 sm:px-6">
+            <div className="relative">
+              <img className="rounded-xl" src={portrait} alt="portrait" />
+              <div className="absolute bottom-0 start-0 -z-[1] translate-y-10 -translate-x-14">
+                <svg
+                  className="max-w-[10rem] h-auto text-blue-200"
+                  width="696"
+                  height="653"
+                  viewBox="0 0 696 653"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="72.5" cy="29.5" r="29.5" fill="#088F8F" />
+                  <circle cx="72.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="171.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="270.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="369.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="468.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="567.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="666.5" cy="29.5" r="29.5" fill="#6495ED" />
+                  <circle cx="29.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="128.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="227.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="326.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="425.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="524.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="623.5" cy="128.5" r="29.5" fill="#6495ED" />
+                  <circle cx="72.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="171.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="270.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="369.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="468.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="567.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="666.5" cy="227.5" r="29.5" fill="#6495ED" />
+                  <circle cx="29.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="128.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="227.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="326.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="425.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="524.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="623.5" cy="326.5" r="29.5" fill="#6495ED" />
+                  <circle cx="72.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="171.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="270.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="369.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="468.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="567.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="666.5" cy="425.5" r="29.5" fill="#6495ED" />
+                  <circle cx="29.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="128.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="227.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="326.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="425.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="524.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="623.5" cy="524.5" r="29.5" fill="#6495ED" />
+                  <circle cx="72.5" cy="623.5" r="29.5" fill="#6495ED" />
+                  <circle cx="171.5" cy="623.5" r="29.5" fill="#6495ED" />
+                  <circle cx="270.5" cy="623.5" r="29.5" fill="#6495ED" />
+                  <circle cx="369.5" cy="623.5" r="29.5" fill="#6495ED" />
+                  <circle cx="468.5" cy="623.5" r="29.5" fill="#6495ED" />
+                  <circle cx="567.5" cy="623.5" r="29.5" fill="#6495ED" />
+                  <circle cx="666.5" cy="623.5" r="29.5" fill="#6495ED" />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <blockquote className="relative">
+              <svg
+                className="absolute top-0 start-0 transform -translate-x-8 -translate-y-4 h-24 w-24 text-gray-200"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  d="M7.39762 10.3C7.39762 11.0733 7.14888 11.7 6.6514 12.18C6.15392 12.6333 5.52552 12.86 4.76621 12.86C3.84979 12.86 3.09047 12.5533 2.48825 11.94C1.91222 11.3266 1.62421 10.4467 1.62421 9.29999C1.62421 8.07332 1.96459 6.87332 2.64535 5.69999C3.35231 4.49999 4.33418 3.55332 5.59098 2.85999L6.4943 4.25999C5.81354 4.73999 5.26369 5.27332 4.84476 5.85999C4.45201 6.44666 4.19017 7.12666 4.05926 7.89999C4.29491 7.79332 4.56983 7.73999 4.88403 7.73999C5.61716 7.73999 6.21938 7.97999 6.69067 8.45999C7.16197 8.93999 7.39762 9.55333 7.39762 10.3ZM14.6242 10.3C14.6242 11.0733 14.3755 11.7 13.878 12.18C13.3805 12.6333 12.7521 12.86 11.9928 12.86C11.0764 12.86 10.3171 12.5533 9.71484 11.94C9.13881 11.3266 8.85079 10.4467 8.85079 9.29999C8.85079 8.07332 9.19117 6.87332 9.87194 5.69999C10.5789 4.49999 11.5608 3.55332 12.8176 2.85999L13.7209 4.25999C13.0401 4.73999 12.4903 5.27332 12.0713 5.85999C11.6786 6.44666 11.4168 7.12666 11.2858 7.89999C11.5215 7.79332 11.7964 7.73999 12.1106 7.73999C12.8437 7.73999 13.446 7.97999 13.9173 8.45999C14.3886 8.93999 14.6242 9.55333 14.6242 10.3Z"
+                  fill="currentColor"
+                />
+              </svg>
+
+              <div className="relative z-10">
+                <p className="text-xs font-semibold text-blue-600 tracking-wide uppercase mb-3 ">
+                  Full-stack Web Developer
+                </p>
+
+                <p className="text-xl font-medium italic text-gray-800 md:text-2xl md:leading-normal xl:text-3xl xl:leading-normal">
+                  Hi, I am Niko! I am passionate full-stack developer interested
+                  in developing solutions for modern frontend and backend
+                  projects.
+                </p>
+              </div>
+
+              <footer className="mt-6">
+                <div className="flex items-center">
+                  <div className="md:hidden flex-shrink-0">
+                    <img
+                      className="h-12 w-12 rounded-full"
+                      src={portrait}
+                      alt="Image Description"
+                    />
+                  </div>
+                </div>
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <main id="content" role="main">
+        <div className="text-center py-10 px-4 sm:px-6 lg:px-8 max-w-5xl mx-auto">
+          <h1 className="block text-2xl font-bold text-black sm:text-4xl">
+            So who am I?
+          </h1>
+          <p className="mt-3 text-lg text-black-300">
+            I come with a rich background of 10 years in television broadcast
+            and photography. My career has allowed me work on small community projects such as Tropfest to
+            large scale projects such as the Australian Open Tennis, Qatar FIFA World Cup 2022, Formula 1 and many more.  
+            <br />
+            <br />
+            Now, I am on an exciting journey, transitioning into
+            the realm of full-stack web development. The learning process,
+            coupled with the thrill of implementing problem-solving solutions,
+            is what truly captivates me in this new venture.
+            <br />
+            <br />
+            I find immense reward in acquiring coding skills and applying them to real-world challenges.
+            My goal is to contribute to upcoming web development solutions and projects, bringing a unique perspective shaped by years in the visual arts.
+            If I am not coding or working I will be spending my town outdoors rock climbing or hiking. 
+          </p>
+        </div>
+
+{/* grid of skills */}
+        <div className="text-center py-1 px-1 sm:px-6 lg:px-8 max-w-5xl mx-auto">
+          <h1 className="block text-2xl font-bold text-black sm:text-4xl">Skills</h1>
+        </div>
+        <div className="min-h-screen flex justify-center">
+        <div className="max-w-[80rem] px-1 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-6 md:gap-10">
+        <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={bootstrap} alt="bootstrap"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Bootstrap</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={express} alt="express"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Expressjs</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={figma} alt="figma"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Figma</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={git} alt="Git"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Git</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={css} alt="css"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">CSS</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={github} alt="Github"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Github</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={heroku} alt="heroku"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Heroku</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={html} alt="HTML"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">HTML</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={javascript} alt="javascript"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Javascript</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={jest} alt="Jest"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Jest</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={jquery} alt="jquery"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Jquery</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={mongodb} alt="MongoDB"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">MongoDB</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={mysql} alt="mySQL"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">mySQL</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={netlify} alt="netlify"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Netlify</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={react} alt="React"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">React</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={Sass} alt="Sass"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Sass</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={Tailwind} alt="Tailwind"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Tailwind</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={Sass} alt="Sass"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Sass</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={graphql} alt="graphQL"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">GraphQL</h3>
+        </div>
+      </div>
+    </div>
+    <div className="w-48 h-20 bg-white shadow-xl rounded-lg p-3">
+      <div className="flex items-center gap-x-4 mb-3">
+        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 ">
+          <img src={redux} alt="Redux"/>
+        </div>
+        <div className="flex-shrink-0">
+          <h3 className="block text-lg font-semibold text-gray-800 ">Redux</h3>
+        </div>
+      </div>
+    </div>
+        </div>
+        </div>
+        </div>
+      </main>
+    </>
+  );
 }
