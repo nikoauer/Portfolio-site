f2dde15c6270f8c8f12bf8a61a96f0b3155251dc
Niko Auer <nikoauer26@gmail.com>
2024-08-16 14:37:46 +1000

    added video background to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,55 +1,34 @@
 import logo from "../assets/portrait.jpg";
 import backdrop from "../assets/backdrop.svg";
-import { useState } from 'react'
-import ArrowDown from "./ArrowDown.jsx"
-
+import ArrowDown from "./ArrowDown.jsx";
+import video from "../assets/homepageVideo.mp4"
 
 export default function HomePage() {
   return (
-    <div className="bg-white">
-
-
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Full Stack developer Python developer
-            </h1>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#about"
-                className="animate-bounce focus:animate-none hover:animate-none inline-flex text-md font-medium mt-3 px-4 py-2 rounded-lg tracking-wide "
-              >
-                <ArrowDown />
-              </a>
-            </div>
+    <div className="relative isolate min-h-screen w-full">
+      {/* <img
+        alt="Backdrop"
+        src={backdrop}
+        className="absolute inset-0 -z-10 h-full w-full object-cover"
+      /> */}
+      <video autoPlay muted loop className="absolute inset-0 -z-10 h-full w-full object-cover">
+        <source src={video} />
+      </video>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            
+          </h1>
+          <div className="mt-[34rem] flex items-center justify-center gap-x-6">
+            <a
+              href="#about"
+              className="animate-bounce focus:animate-none hover:animate-none inline-flex text-md font-medium mt-3 px-4 py-2 rounded-lg tracking-wide"
+            >
+              <ArrowDown />
+            </a>
           </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
         </div>
       </div>
     </div>
-  )
+  );
 }
