4444169979e139515a0d88afbdd2356f191ddeef
Niko Auer <nikoauer26@gmail.com>
2024-08-16 12:41:28 +1000

    customized initial loading page and further customized navigation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,55 @@
 import logo from "../assets/portrait.jpg";
-import AboutPage from "./AboutPage";
+import backdrop from "../assets/backdrop.svg";
+import { useState } from 'react'
+import ArrowDown from "./ArrowDown.jsx"
+
 
 export default function HomePage() {
-
   return (
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-  <div className="w-max text-center">
-    <h1
-      className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black pr-5 text-5xl text-black font-bold">
-      Hello World
-    </h1>
-  </div>
-</div>
-  );
+    <div className="bg-white">
+
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Full Stack developer Python developer
+            </h1>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#about"
+                className="animate-bounce focus:animate-none hover:animate-none inline-flex text-md font-medium mt-3 px-4 py-2 rounded-lg tracking-wide "
+              >
+                <ArrowDown />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,80 +1,85 @@
 import logo from "../assets/logo3.svg";
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, DialogPanel } from '@headlessui/react'
 
+const navigation = [
+  { name: 'About', href: '#about' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Contact', href: '#contact' },
+]
 export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <Disclosure as="nav" className="bg-white shadow">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-24 items-center justify-between">
-          <div className="flex items-center">
-            <img alt="Niko Auer" src={logo} className="h-16 w-auto" />
-          </div>
-          <div className="hidden sm:flex sm:space-x-8">
-            <a
-              className="font-medium text-gray-700 hover:text-gray-500 sm:py-6"
-              href="#about"
-            >
-              About
-            </a>
-            <a
-              className="font-medium text-gray-700 hover:text-gray-500 sm:py-6"
-              href="#projects"
-            >
-              Projects
-            </a>
-            <a
-              className="font-medium text-gray-700 hover:text-gray-500 sm:py-6"
-              href="#contact"
-            >
-              Contact
-            </a>
-          </div>
-          <div className="-mr-2 flex items-center sm:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon
-                aria-hidden="true"
-                className="block h-6 w-6 group-data-[open]:hidden"
-              />
-              <XMarkIcon
-                aria-hidden="true"
-                className="hidden h-6 w-6 group-data-[open]:block"
-              />
-            </DisclosureButton>
-          </div>
-        </div>
+    <header className="absolute inset-x-0 top-0 z-50">
+    <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+      <div className="flex lg:flex-1">
+        <a href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Niko Auer</span>
+          <img
+            alt="Niko Auer"
+            src={logo}
+            className="h-16 w-auto"
+          />
+        </a>
+      </div>
+      <div className="flex lg:hidden">
+        <button
+          type="button"
+          onClick={() => setMobileMenuOpen(true)}
+          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        >
+          <span className="sr-only">Open main menu</span>
+          <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+        </button>
+      </div>
+      <div className="hidden lg:flex lg:gap-x-12">
+        {navigation.map((item) => (
+          <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+            {item.name}
+          </a>
+        ))}
       </div>
 
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 pb-3 pt-2">
-          <DisclosureButton
-            as="a"
-            href="#about"
-            className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+    </nav>
+    <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+      <div className="fixed inset-0 z-50" />
+      <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="flex items-center justify-between">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Niko Auer</span>
+            <img
+              alt="Niko Auer"
+              src={logo}
+              className="h-16 w-auto"
+            />
+          </a>
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(false)}
+            className="-m-2.5 rounded-md p-2.5 text-gray-700"
           >
-            About
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#projects"
-            className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >
-            Projects
-          </DisclosureButton>
-          <DisclosureButton
-            as="a"
-            href="#contact"
-            className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >
-            Contact
-          </DisclosureButton>
+            <span className="sr-only">Close menu</span>
+            <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+          </button>
         </div>
-      </DisclosurePanel>
-    </Disclosure>
+        <div className="mt-6 flow-root">
+          <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="space-y-2 py-6">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
   );
 }
