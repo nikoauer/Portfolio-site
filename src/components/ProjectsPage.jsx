8c11d8fd489ccd8637c0c712e430d5035ccc4c2e
Niko Auer <nikoauer26@gmail.com>
2024-09-01 14:33:25 +1000

    added project data to projectspage.jsx

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -5,43 +5,41 @@ import projectstitle from "../assets/projectstitle.svg";
 
 const Projects = [
   {
-    projectName: "Rockfit",
+    projectName: "Rockfit - Rock climbing shoe calculator",
     description:
       "A rock climbing shoe calculator that was designed to assist a rock climbing retailer in being able to provide an easy to use shoe guide for customers that want to purchase shoes online.",
-    tools: "Expressjs, Nodejs, MongoDB, React, Bootstrap.",
+    tools: "Expressjs, Nodejs, MongoDB, React, Bootstrap",
     imgSrc: rockfit,
     GithubLink: "https://github.com/nikoauer/Rock-Climbing-Shoe-Calculator",
     LiveLink: "https://shoe-calculator-frontend.onrender.com/",
     icon: WrenchIcon,
   },
   {
-    projectName: "Rockfit",
+    projectName: "Elementum - Adventure E-commerce Store",
     description:
-      "A rock climbing shoe calculator that was designed to assist a rock climbing retailer in being able to provide an easy to use shoe guide for customers that want to purchase shoes online.",
-    tools: "Expressjs, Nodejs, MongoDB, React, Bootstrap.",
+      "An adventure E-commerce store that allows customers to purchase goods through Paypal. Additionally if a user is an admin it allows them to add, remove and change products, users and ",
+    tools: "Expressjs, MongoDB, Tailwind CSS, React, Nodejs",
     imgSrc: rockfit,
-    GithubLink: "https://github.com/nikoauer/Rock-Climbing-Shoe-Calculator",
-    LiveLink: "https://shoe-calculator-frontend.onrender.com/",
+    GithubLink: "https://github.com/nikoauer/Elementum-E-Commerce-Store",
     icon: WrenchIcon,
   },
   {
-    projectName: "Rockfit",
+    projectName: "ChatApp - Messenging site",
     description:
-      "A rock climbing shoe calculator that was designed to assist a rock climbing retailer in being able to provide an easy to use shoe guide for customers that want to purchase shoes online.",
-    tools: "Expressjs, Nodejs, MongoDB, React, Bootstrap.",
+      "A messenging application that allows users to send messages to anyone on the application (catered towards being used as an internal work messaging application) or create group chats.",
+    tools: "React, GetStream API, Tailwind CSS, ",
     imgSrc: rockfit,
-    GithubLink: "https://github.com/nikoauer/Rock-Climbing-Shoe-Calculator",
-    LiveLink: "https://shoe-calculator-frontend.onrender.com/",
+    GithubLink: "https://github.com/nikoauer/ChatApplication",
+    LiveLink: "https://chatapplication-frontend-cqtl.onrender.com/",
     icon: WrenchIcon,
   },
   {
-    projectName: "Rockfit",
+    projectName: "Flight deal checker",
     description:
-      "A rock climbing shoe calculator that was designed to assist a rock climbing retailer in being able to provide an easy to use shoe guide for customers that want to purchase shoes online.",
-    tools: "Expressjs, Nodejs, MongoDB, React, Bootstrap.",
+      "A python script that retrieves the capital cities of countries I wish to travel to from a Google sheet. Populates this google sheet with IATA codes if not already added and proceeds to check the Amadeus API for flights to these cities that are under my desired maximum price. If a flight is found within my desired criteria it will proceed to sending me a WhatsApp message to notify me that a flight has been found with my desired criteria.",
+    tools: "Python, Google Sheets, Amadeus API, Twilio, Requests: HTTP for Humans",
     imgSrc: rockfit,
-    GithubLink: "https://github.com/nikoauer/Rock-Climbing-Shoe-Calculator",
-    LiveLink: "https://shoe-calculator-frontend.onrender.com/",
+    GithubLink: "https://github.com/nikoauer/Flight-deal-tracker",
     icon: WrenchIcon,
   },
 ];
